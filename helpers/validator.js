c5f2dd7379003eb947e6b57e8929352bdd8bf4b7
danilsahuta <danil.saxuta@icloud.com>
2021-10-21 14:22:35 +0300

    feat: add validation

--- helpers/validator.js
+++ helpers/validator.js
@@ -2,7 +2,25 @@ class Validator{
     static isEmpty(value) {
         if (value === undefined) return true;
 
+        if (
+            typeof value == "function" ||
+            typeof value == "number" ||
+            typeof value == "boolean" ||
+            Object.prototype.toString.call.value === "[object Date]"
+        ) return false;
 
+        if (value === null || value.length === 0) return true;
 
+        return false;
+    }
+
+    static isInteger(value) {
+        return (value ^ 0) === value;
+    }
+
+    static isIn(value, array) {
+        return array.includes(value);
     }
 }
+
+module.exports = Validator;
