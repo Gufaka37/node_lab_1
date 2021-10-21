c07155a5b909fe583f88ee692c53b80f9e709076
danilsahuta <danil.saxuta@icloud.com>
2021-10-21 16:43:19 +0300

    feat: written code for function: stringOperation

---
+++ helpers/mainOperations.js
@@ -0,0 +1,46 @@
+function stringOperation(str) {
+    const numbers = Number(str);
+    let array;
+    let numbersArray = [];
+    let newArray = [];
+
+    if (Number.isInteger(numbers)) {
+        array = str.match(/[0-9]/g);
+        array.forEach(number => {
+            numbersArray.push(Number(number));
+        });
+
+        numbersArray.forEach(number => {
+            if (number % 2) {
+                if (numbersArray.indexOf(number) === 0) {
+                    newArray.push(number.toString());
+                    newArray.push("-");
+                } else if ((numbersArray.indexOf(number) === numbersArray.length - 1)) {
+                    if (newArray[newArray.length - 1] === "-") {
+                        newArray.push(number.toString());
+                    } else {
+                        newArray.push("-");
+                        newArray.push(number.toString());
+                    }
+                } else {
+                    if (newArray[newArray.length - 1] === "-") {
+                        newArray.push(number.toString());
+                        newArray.push("-");
+                    } else {
+                        newArray.push("-");
+                        newArray.push(number.toString());
+                        newArray.push("-");
+                    }
+                }
+            } else {
+                newArray.push(number.toString());
+            }
+        });
+        return newArray.join('');
+    } else if (isNaN(str)) {
+        return NaN
+    }
+    return null;
+}
+
+module.exports.stringOperation = stringOperation;
